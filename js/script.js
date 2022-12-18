932051367d914c40838f6b1569eea73c7d695b40
romanjhyltsou <romanjhyltsou@gmail.com>
2022-12-18 21:14:32 +0300

    init:start project

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/* const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d'); //default width 300px height 150px
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ */
+
+/* function animate(timestamp) {
+requestAnimationFrame(animate);
+
+}
+animate(0);
+ */
