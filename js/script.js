ad194ddb3d4154938beefe095b7c77e7290fc610
romanjhyltsou <romanjhyltsou@gmail.com>
2022-12-23 00:21:35 +0300

    feat: adde reset the game, mobile version and reset the game, mobile version

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 window.addEventListener('load', function(){
   const canvas = document.getElementById('canvas1');
   const ctx = canvas.getContext('2d'); //default width 300px height 150px
-  canvas.width = 800;
+  canvas.width = 1300;
   canvas.height = 720;
   let enemies = [];
   let score = 0;
   let gameOver = false;
+  const fullScreenButton = this.document.getElementById('fullScreenButton');
 
   class InputHandler {
     constructor() {
       this.keys = [];
+      this.touchY = '';
+      this.touchTreshold = 30;
       window.addEventListener('keydown', e =>{
         if((e.key === 'ArrowDown' ||
            e.key === 'ArrowUp' ||
@@ -17,8 +20,7 @@ window.addEventListener('load', function(){
            e.key === 'ArrowRight')
            && this.keys.indexOf(e.key) === -1){
           this.keys.push(e.key);
-        }
-        console.log(e.key, this.keys);
+        }else if (e.key === 'Enter' && gameOver) restartGame(); //new game
       });
       window.addEventListener('keyup', e =>{
         if(e.key === 'ArrowDown' ||
@@ -29,7 +31,22 @@ window.addEventListener('load', function(){
         }
         console.log(e.key, this.keys);
       });
-      
+      window.addEventListener('touchstart', e => {
+        this.touchY = e.changedTouches[0].pageY;
+      });
+      window.addEventListener('touchmove', e => {
+        const swipeDistance = e.changedTouches[0].pageY - this.touchY;
+        if(swipeDistance < -this.touchTreshold && this.keys.indexOf('swipe up') === -1) this.keys.push('swipe up');
+        else if (swipeDistance > this.touchTreshold && this.keys.indexOf('swipe down') === -1) {
+          this.keys.push('swipe down');
+          if (gameOver) restartGame();
+        } 
+
+      });
+      window.addEventListener('touchend', e => {
+        this.keys.splice(this.keys.indexOf('swipe up'), 1);
+        this.keys.splice(this.keys.indexOf('swipe down'), 1);
+      });
     }
 
   }
@@ -39,7 +56,7 @@ window.addEventListener('load', function(){
       this.gameHeight = gameHeight;
       this.width = 200;
       this.height = 200;
-      this.x = 0;
+      this.x = 100;
       this.y = this.gameHeight - this.height;
       this.image = document.getElementById('playerImage');
       this.frameX = 0;
@@ -53,6 +70,13 @@ window.addEventListener('load', function(){
       this.weight = 1;
     }
 
+    restart() {
+      this.x = 100;
+      this.y = this.gameHeight - this.height;
+      this.maxFrame = 8;
+      this.frameY = 0;
+    }
+
     draw(context){
       context.drawImage(this.image,this.frameX * this.width,
       this.frameY * this.height,this.width, this.height, this.x, this.y,this.width, this.height);
@@ -61,10 +85,10 @@ window.addEventListener('load', function(){
     update(input, deltaTime, enemies) {
       // collision detection
       enemies.forEach(enemy => {
-        const dx = (enemy.x + enemy.width/2) - (this.x + this.width/2);
-        const dy = (enemy.y + enemy.height/2) - (this.y + enemy.height/2);
+        const dx = (enemy.x + enemy.width/2 - 20) - (this.x + this.width/2);
+        const dy = (enemy.y + enemy.height/2) - (this.y + enemy.height/2 + 20);
         const distance = Math.sqrt(dx * dx + dy * dy);
-        if(distance < enemy.width/2 + this.width/2){
+        if(distance < enemy.width/3 + this.width/3){
           gameOver = true;
         }
       });
@@ -81,7 +105,7 @@ window.addEventListener('load', function(){
         this.speed = 5;
       }else if(input.keys.indexOf('ArrowLeft') > -1){
         this.speed = -5;
-      }else if(input.keys.indexOf('ArrowUp') > -1 && this.onGround()){
+      }else if ((input.keys.indexOf('ArrowUp') > -1 || input.keys.indexOf('swipe up') > -1 ) && this.onGround()){
         this.vy -= 32;
       }else {
         this.speed = 0;
@@ -127,6 +151,10 @@ window.addEventListener('load', function(){
     update() {
       this.x -= this.speed;
       if(this.x < 0 - this.width) this.x = 0;
+    }
+
+    restart() {
+      this.x = 0;
     }
   }
   class Enemy {
@@ -188,6 +216,7 @@ window.addEventListener('load', function(){
   }
 
   function displayStatusText(context) {
+    context.textAlign = 'left';
     context.font = '40px Helvetica';
     context.fillStyle = 'black';
     context.fillText('Score: ' + score, 20, 50);
@@ -196,11 +225,32 @@ window.addEventListener('load', function(){
     if(gameOver){
       context.textAlign = 'center';
       context.fillStyle = 'black';
-      context.fillText('GAME OVER, try again!: ', canvas.width/2, 200);
+      context.fillText('GAME OVER, press Enter or swipe down to restart', canvas.width/2, 200);
       context.fillStyle = 'white';
-      context.fillText('GAME OVER, try again!: ', canvas.width/2 + 2, 202);
+      context.fillText('GAME OVER, press Enter or swipe down to restart', canvas.width/2 + 2, 202);
     }
   }
+
+  function restartGame(){
+    player.restart();
+    background.restart();
+    enemies = [];
+    score = 0;
+    gameOver = false;
+    animate(0);
+  }
+
+  function toggleFullScreen() {
+    if(!document.fullscreenElement){
+      canvas.requestFullscreen().catch(err => {
+        alert(`Error, can't enable full-screen mode: ${err.massage}`);
+      });
+    } else {
+      document.exitFullscreen();
+    }
+  }
+  fullScreenButton.addEventListener('click', toggleFullScreen);
+
 const input = new InputHandler();
 const player = new Player(canvas.width, canvas.height);
 const background = new Background(canvas.weight, canvas.height);
@@ -227,24 +277,3 @@ function animate(timeStamp) {
 animate(0);
 
 });
-
-
-const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
-
-function sortStudentsByGroups(arr) {
-  let arr1 = [];
-  arr.sort();
-  for(let i = 0; i < arr.length; i++){
-    if(i === 2) {
-      arr1.push(arr.splice(0, 3));
-      i = 0;
-    }
-    if(arr.length < 2){
-      arr1.push(`Оставшиеся студенты: ${arr.length === 0 ? '-' : arr.join(', ')}`);
-    }
-    
-  }
-  console.log(arr1);
-}
-
-sortStudentsByGroups(students);
